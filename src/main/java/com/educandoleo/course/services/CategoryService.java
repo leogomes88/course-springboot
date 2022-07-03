ceb4f620a5c5107dc852e15eb279f848685519b2
leogomes88 <leonard56gle@gmail.com>
2022-07-02 22:29:24 -0300

    Category

---
+++ src/main/java/com/educandoleo/course/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.educandoleo.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoleo.course.entities.Category;
+import com.educandoleo.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
